95824cc3a76ec320e8042b8c18ad60f1c4482e43
mohamadaffouri <81240330+mohamadaffouri@users.noreply.github.com>
2024-08-26 19:58:36 +0300

    change logo for dahsboard

--- rentalSystem/resources/views/layouts/dash.blade.php
+++ rentalSystem/resources/views/layouts/dash.blade.php
@@ -27,7 +27,11 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="{{ url('/') }}"><img src="{{ asset('images/logo.svg') }}" class="mr-2" alt="logo"/></a>
+       <div class="logo">
+
+                <img src="img/logo1.png" alt="">
+
+        </div>
         <a class="navbar-brand brand-logo-mini" href="{{ url('/') }}"><img src="{{ asset('images/logo-mini.svg') }}" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
