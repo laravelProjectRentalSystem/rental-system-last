ed185c7551cf2096cd5df6e7942466ab9bb6417a
mohamadaffouri <81240330+mohamadaffouri@users.noreply.github.com>
2024-08-26 20:12:58 +0300

    add default img to manage new user section

--- rentalSystem/resources/views/layouts/dashB.blade.php
+++ rentalSystem/resources/views/layouts/dashB.blade.php
@@ -28,7 +28,11 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="{{ url('/') }}"><img src="{{ asset('images/logo.svg') }}" class="mr-2" alt="logo"/></a>
+        <div class="logo">
+
+            <img src="img/logo1.png" alt="">
+
+    </div>
         <a class="navbar-brand brand-logo-mini" href="{{ url('/') }}"><img src="{{ asset('images/logo-mini.svg') }}" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
@@ -37,14 +41,7 @@
         </button>
         <ul class="navbar-nav mr-lg-2">
           <li class="nav-item nav-search d-none d-lg-block">
-            <div class="input-group">
-              <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
-                <span class="input-group-text" id="search">
-                  <i class="icon-search"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
-            </div>
+
           </li>
         </ul>
         <ul class="navbar-nav navbar-nav-right">
@@ -100,11 +97,7 @@
               </a>
             </div>
           </li>
-          <li class="nav-item nav-settings d-none d-lg-flex">
-            <a class="nav-link" href="#">
-              <i class="icon-ellipsis"></i>
-            </a>
-          </li>
+
         </ul>
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
           <span class="icon-menu"></span>
